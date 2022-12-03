cf78762edab6104ae8756b0e9bb0f1c3c913ef9f
nasaveli <116846576+nasaveli@users.noreply.github.com>
2022-12-03 18:36:13 -0500

    initial commit

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,13 +1,21 @@
 import React from "react";
-import SignUpForm from "../../components/SignUpForm/SignUpForm";
-import LoginForm from "../../components/LoginForm/LoginForm"
+// import SignUpForm from "../../components/SignUpForm/SignUpForm";
+// import LoginForm from "../../components/LoginForm/LoginForm"
 
 export default function AuthPage({ setUser }) {
   return (
-    <>
-      <h1>AuthPage</h1>
-      <SignUpForm setUser={setUser} />
-      <LoginForm setUser={setUser} />
-    </>
+    <main className="authContainer">
+          <h1 className="authTitle">Artspiration</h1>
+          <div className="authImage">
+            <img src="" alt="" />
+          </div>
+          <div className="authBtnContatiner">
+            <button className="signUpBtn">Sign Up</button>
+            <button className="logInBtn">Log In</button>
+          </div>
+      {/* <SignUpForm setUser={setUser} />
+      <LoginForm setUser={setUser} /> */}
+    
+    </main>
   );
 }
