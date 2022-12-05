fe2a308d80774f5deec53fc39147db87a6c1d361
raeganmb <raeganboucher2@gmail.com>
2022-12-05 13:16:26 -0400

    Create Auth css and fix styling

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -2,13 +2,14 @@ import React from "react";
 // import SignUpForm from "../../components/SignUpForm/SignUpForm";
 // import LoginForm from "../../components/LoginForm/LoginForm";
 import { Link } from "react-router-dom";
+import "./AuthPage.css"
 
 export default function AuthPage() {
   return (
     <main className="authContainer">
-          <h1 className="authTitle">Artspiration</h1>
+          <h1 className="authTitle">Artspiration.</h1>
           <div className="authImage">
-            <img src="https://via.placeholder.com/150x200.png" alt="" />
+            {/* <img src="https://via.placeholder.com/150x200.png" alt="" /> */}
           </div>
           <div className="authBtnContatiner">
             <button className="signUpBtn"> <Link to="/signup">Sign Up</Link></button>
