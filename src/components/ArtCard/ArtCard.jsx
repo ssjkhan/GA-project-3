481fd41a16edc8aa27c7690dbe814fc82efceb2f
raeganmb <raeganboucher2@gmail.com>
2022-12-06 18:52:59 -0400

    added cards and layout components

---
+++ src/components/ArtCard/ArtCard.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./ArtCard.css"
+
+export default function ArtCard(props) {
+    return (
+        <div style={{
+            ...styles.card,
+            ...styles[props.size]
+        }}>
+        </div>
+    )
+}
+
+const styles = {
+    card: {
+        margin: '15px 10px',
+        padding: 0,
+        borderRadius: '16px',
+        backgroundColor: 'red'
+    },
+    small: {
+        gridRowEnd: 'span 26'
+    },
+    medium: {
+        gridRowEnd: 'span 33'
+    },
+    large: {
+        gridRowEnd: 'span 45'
+    }
+}
---
+++ src/components/CollectionsLayout/CollectionsLayout.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ArtCard from "../ArtCard/ArtCard.jsx";
+import "./CollectionsLayout.css"
+
+export default function CollectionsLayout() {
+    return (
+        <div className="pinContainer">
+            <ArtCard />
+        </div>
+    )
+}
