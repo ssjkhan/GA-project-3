3f93a7581471b4cb05486229dbdec9d1ec2dff70
raeganmb <raeganboucher2@gmail.com>
2022-12-07 12:59:00 -0400

    collections layout

--- src/components/ArtCard/ArtCard.jsx
+++ src/components/ArtCard/ArtCard.jsx
@@ -1,30 +1,31 @@
 import React from "react";
-import "./ArtCard.css"
+import "./ArtCard.css";
 
 export default function ArtCard(props) {
-    return (
-        <div style={{
-            ...styles.card,
-            ...styles[props.size]
-        }}>
-        </div>
-    )
+  return (
+    <div
+      style={{
+        ...styles.card,
+        ...styles[props.size],
+      }}
+    ></div>
+  );
 }
 
 const styles = {
-    card: {
-        margin: '15px 10px',
-        padding: 0,
-        borderRadius: '16px',
-        backgroundColor: 'red'
-    },
-    small: {
-        gridRowEnd: 'span 26'
-    },
-    medium: {
-        gridRowEnd: 'span 33'
-    },
-    large: {
-        gridRowEnd: 'span 45'
-    }
-}
+  card: {
+    margin: "15px 10px",
+    padding: 0,
+    borderRadius: "16px",
+    backgroundColor: "red",
+  },
+  small: {
+    gridRowEnd: "span 26",
+  },
+  medium: {
+    gridRowEnd: "span 33",
+  },
+  large: {
+    gridRowEnd: "span 45",
+  },
+};
