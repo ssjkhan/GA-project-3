23e08d8371ad2304e4082ad3cdd1aab927fae9c5
chanjongpak <chanjong.pak@gmail.com>
2022-12-07 18:53:40 -0500

    working on it

--- controllers/artwork.js
+++ controllers/artwork.js
@@ -4,35 +4,31 @@ const controller = "ARTWORK";
 
 // get artwork data as json
 async function getArtwork(req, resp, next) {
-	if (req.params.artwork_id === "artwork_id") {
-		var params = {
-			artwork_id: "516dfb9ab31e2b2270000c45",
-		};
-	}
+  if (req.params.artwork_id === "artwork_id") {
+    var params = {
+      artwork_id: "516dfb9ab31e2b2270000c45",
+    };
+  }
 
-	var result = await API.getArtwork(params);
-	resp.send(result);
+  var result = await API.getArtwork(params);
+  resp.send(result);
 }
 
 async function saveArtwork(req, res, next) {
-	res.send({
-		controller: controller,
-		url: req.originalUrl,
-		params: req.params,
-	});
+  console.log("hit function");
 }
 
 async function getRandomArtwork(req, resp, next) {
-	let params = {
-		offset: Math.floor(Math.random() * 50),
-	};
-	let result = await API.getArtwork(params);
+  let params = {
+    offset: Math.floor(Math.random() * 50),
+  };
+  let result = await API.getArtwork(params);
 
-	resp.send(result);
+  resp.send(result);
 }
 
 module.exports = {
-	getArtwork,
-	saveArtwork,
-	getRandomArtwork,
+  getArtwork,
+  saveArtwork,
+  getRandomArtwork,
 };
