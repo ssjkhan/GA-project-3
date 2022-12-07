a7de7c91c27186ce07ad7b27b3fb33755fbfd9f9
chanjongpak <chanjong.pak@gmail.com>
2022-12-07 15:09:56 -0500

    show functionality

--- src/utilities/artsy-api-service.js
+++ src/utilities/artsy-api-service.js
@@ -1,7 +1,8 @@
-async function randomArt() {
-  const data = {};
+const axios = require("axios");
+
+export async function randomArt() {
   try {
-    const res = await axios.get("htttp://localhost:3001/artwork/random", data);
+    const res = await axios.get("http://localhost:3001/artwork/random");
     return res.data;
   } catch (err) {
     console.log(err);
