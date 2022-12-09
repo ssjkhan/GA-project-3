6ce5dfefe4c12cea6742d9cbbcd8a42c9dc65430
chanjongpak <chanjong.pak@gmail.com>
2022-12-09 00:43:52 -0500

    final changes

--- controllers/artwork.js
+++ controllers/artwork.js
@@ -8,66 +8,66 @@ const mongoose = require("mongoose");
 
 // get artwork data as json
 async function getArtwork(req, resp, next) {
-	if (req.params.artwork_id === "artwork_id") {
-		var params = {
-			artwork_id: "516dfb9ab31e2b2270000c45",
-		};
-	}
+  if (req.params.artwork_id === "artwork_id") {
+    var params = {
+      artwork_id: "516dfb9ab31e2b2270000c45",
+    };
+  }
 
-	var result = await API.getArtwork(params);
-	resp.send(result);
+  var result = await API.getArtwork(params);
+  resp.send(result);
 }
 
 async function saveArtwork(req, res, next) {
-	let artwork_def = {
-		artwork_id: req.query.artwork_id,
-	};
+  let artwork_def = {
+    artwork_id: req.query.artwork_id,
+  };
 
-	try {
-		// artwork
-		let dbQuery = await Artwork.find(artwork_def);
+  try {
+    // artwork
+    let dbQuery = await Artwork.find(artwork_def);
 
-		if (!dbQuery[0]) {
-			let artwork_data = await API.getArtwork(artwork_def);
-			artwork_data["artwork_id"] = artwork_data.id;
-			var artwork = new Artwork(artwork_data);
-		} else {
-			var artwork = dbQuery[0];
-		}
-		await artwork.save();
+    if (!dbQuery[0]) {
+      let artwork_data = await API.getArtwork(artwork_def);
+      artwork_data["artwork_id"] = artwork_data.id;
+      var artwork = new Artwork(artwork_data);
+    } else {
+      var artwork = dbQuery[0];
+    }
+    await artwork.save();
 
-		// gallery
-		var searchRes = await Gallery.find({
-			user: new mongoose.Types.ObjectId(req.query.user_id),
-		});
+    // gallery
+    var searchRes = await Gallery.find({
+      user: new mongoose.Types.ObjectId(req.query.user_id),
+    });
 
-		gallery = searchRes[0];
-		if (!gallery) {
-			gallery = new Gallery({
-				user: req.query.user_id,
-			});
+    gallery = searchRes[0];
+    if (!gallery) {
+      gallery = new Gallery({
+        user: req.query.user_id,
+      });
 
-			await gallery.save();
-		}
+      await gallery.save();
+    }
 
-		gallery.artworks.push(artwork);
-		gallery.save();
-	} catch (error) {
-		console.log(error);
-	}
+    gallery.artworks.push(artwork);
+    gallery.save();
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 async function getRandomArtwork(req, resp, next) {
-	let params = {
-		offset: Math.floor(Math.random() * 50),
-	};
-	let result = await API.getArtwork(params);
+  let params = {
+    offset: Math.floor(Math.random() * 50),
+  };
+  let result = await API.getArtwork(params);
 
-	resp.send(result);
+  resp.send(result);
 }
 
 module.exports = {
-	getArtwork,
-	saveArtwork,
-	getRandomArtwork,
+  getArtwork,
+  saveArtwork,
+  getRandomArtwork,
 };
