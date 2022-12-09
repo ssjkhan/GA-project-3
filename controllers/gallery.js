efd8adf455461a221b3482bc7bbee51fea993e90
chanjongpak <chanjong.pak@gmail.com>
2022-12-09 01:59:09 -0500

    more changes

--- controllers/gallery.js
+++ controllers/gallery.js
@@ -6,48 +6,48 @@ const controller = "GALLERY";
 
 // get a single gallery by ID
 async function getGallery(req, resp, next) {
-	let gallery = await Gallery.find({
-		user: new mongoose.Types.ObjectId(req.query.user_id),
-	}).populate("artworks");
+  let gallery = await Gallery.find({
+    user: new mongoose.Types.ObjectId(req.query.user_id),
+  }).populate("artworks");
 
-	if (gallery[0]) {
-		resp.send(
-			gallery[0].artworks.map((artwork) => {
-				artwork["_id"] = artwork["_id"].toString();
-				return artwork;
-			})
-		);
-	} else {
-		let user = await User.findById("63923eb516fc5aabe531afe1");
-		gallery = new Gallery({ user: user });
-		await gallery.save();
-		resp.send([]);
-	}
+  if (gallery[0]) {
+    resp.send(
+      gallery[0].artworks.map((artwork) => {
+        artwork["_id"] = artwork["_id"].toString();
+        return artwork;
+      })
+    );
+  } else {
+    let user = await User.findById("63923eb516fc5aabe531afe1");
+    gallery = new Gallery({ user: user });
+    await gallery.save();
+    resp.send([]);
+  }
 }
 
 // remove artwork from the gallery
 async function removeArtwork(req, res, next) {
-	console.log(req.query);
-	try {
-		let dbQuery = await Gallery.find({
-			user: mongoose.Types.ObjectId(req.query.user_id),
-		}).populate("artworks");
+  console.log(req.query);
+  try {
+    let dbQuery = await Gallery.find({
+      user: mongoose.Types.ObjectId(req.query.user_id),
+    }).populate("artworks");
 
-		let gallery = dbQuery[0];
+    let gallery = dbQuery[0];
 
-		console.log("Count:\t" + gallery.artworks.length);
+    console.log("Count:\t" + gallery.artworks.length);
 
-		let artworks = gallery.artworks.filter((artwork, ind) => {
-			console.log("ind\t" + artwork.artwork_id);
-			return artwork.artwork_id != req.query.artwork_id;
-		});
+    let artworks = gallery.artworks.filter((artwork, ind) => {
+      console.log("ind\t" + artwork.artwork_id);
+      return artwork.artwork_id != req.query.artwork_id;
+    });
 
-		const update = { artworks: artworks };
-		await gallery.updateOne(update);
-	} catch (error) {}
+    gallery.artworks = artworks;
+    await gallery.save();
+  } catch (error) {}
 }
 
 module.exports = {
-	getGallery,
-	removeArtwork,
+  getGallery,
+  removeArtwork,
 };
--- src/pages/BrowseArtPage/BrowseArtPage.jsx
+++ src/pages/BrowseArtPage/BrowseArtPage.jsx
@@ -7,59 +7,52 @@ import { getUser } from "../../utilities/users-service.js";
 import { useCallback, useReducer } from "react";
 
 export default function BrowseArtPage() {
-	const [loading, setLoading] = useState(true);
-	const [href, setHref] = useState("");
-	const [id, setId] = useState("");
-	const hrefFetchedRef = useRef(false);
+  const [loading, setLoading] = useState(true);
+  const [href, setHref] = useState("");
+  const [id, setId] = useState("");
+  const hrefFetchedRef = useRef(false);
 
-	async function getArt() {
-		let result = await apiService.randomArt();
-		setHref(result[0].image_details.thumbnail.href);
-		setLoading(false);
-		setId(result[0].id);
-	}
+  async function getArt() {
+    let result = await apiService.randomArt();
+    setHref(result[0].image_details.thumbnail.href);
+    setLoading(false);
+    setId(result[0].id);
+  }
 
-	async function saveArt(event) {
-		event.preventDefault();
-		let artwork_id = event.target[0].value;
-		let user_id = getUser()._id;
-		apiService.saveArt(artwork_id, user_id);
-	}
+  async function saveArt(event) {
+    event.preventDefault();
+    let artwork_id = event.target[0].value;
+    let user_id = getUser()._id;
+    apiService.saveArt(artwork_id, user_id);
+  }
 
-	useEffect(() => {
-		if (hrefFetchedRef.current) return;
-		hrefFetchedRef.current = true;
-		getArt();
-	}, []);
+  useEffect(() => {
+    if (hrefFetchedRef.current) return;
+    hrefFetchedRef.current = true;
+    getArt();
+  }, []);
 
-	return (
-		<>
-			<div>
-				<h1>Here is your Random Artspiration:</h1>
-				{loading ? <p>loading</p> : <img src={href} />}
-			</div>
-			{!loading ? (
-				<div>
-					<button onClick={getArt}>More Artspiration!</button>
-					<form className="formBtn" onSubmit={saveArt}>
-						<input
-							type="hidden"
-							name="artwork_id"
-							value={id}
-						></input>
-						<button
-							type="submit"
-							onClick={getArt}
-						>
-							Save to my Gallery
-						</button>
-					</form>
-				</div>
-			) : (
-				<div>
-					<button onClick={getArt}>Random Artspiration</button>
-				</div>
-			)}
-		</>
-	);
+  return (
+    <>
+      <div>
+        <h1>Here is your Random Artspiration:</h1>
+        {loading ? <p>loading</p> : <img src={href} />}
+      </div>
+      {!loading ? (
+        <div>
+          <button onClick={getArt}>More Artspiration!</button>
+          <form className="formBtn" onSubmit={saveArt}>
+            <input type="hidden" name="artwork_id" value={id}></input>
+            <button type="submit" onClick={getArt}>
+              Save to my Gallery
+            </button>
+          </form>
+        </div>
+      ) : (
+        <div>
+          <button onClick={getArt}>Random Artspiration</button>
+        </div>
+      )}
+    </>
+  );
 }
